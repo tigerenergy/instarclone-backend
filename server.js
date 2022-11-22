39d9600941ee91b89f6720496c29e69fca1a8c8b
kiwoon kim <kisoon0947@gmail.com>
2022-11-22 20:59:08 +0900

    ApolloServer test

--- server.js
+++ server.js
@@ -1,14 +1,34 @@
 import { ApolloServer, gql } from 'apollo-server';
 
 const typeDefs = gql`
+  type Movie {
+    title: String
+    year: Int
+  }
   type Query {
-    hello: String
+    movies: [Movie]
+    movie: Movie
+  }
+  type Mutation {
+    createMovie(title: String!): Boolean
+    deleteMovie(title: String!): Boolean
   }
 `;
 
 const resolvers = {
   Query: {
-    hello: () => 'test',
+    movies: () => [],
+    movie: () => ({ title: 'hello', year: 2021 }),
+  },
+  Mutation: {
+    createMovie: (_, title) => {
+      console.log(title);
+      return true;
+    },
+    deleteMovie: (_, title) => {
+      console.log(title);
+      return true;
+    },
   },
 };
 
