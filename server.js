392e3ff744499d14554870bc7b6808969c74b2ff
kiwoon kim <kisoon0947@gmail.com>
2022-11-23 21:45:27 +0900

    update

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
+require('dotenv').config();
 import { ApolloServer } from 'apollo-server';
 import schema from './schema';
 const server = new ApolloServer({
   schema,
 });
 
-server.listen().then(() => console.log('Server is running on http://localhost:4000/'));
+const PORT = process.env.PORT;
+
+server.listen(PORT).then(() => console.log(`🎊 Server is running on http://localhost:${PORT}/`));
