9bbf3e036edd9fb91bcf3fa30f8388fd76da53a8
kiwoon kim <kisoon0947@gmail.com>
2022-11-25 20:42:11 +0900

    authentication

--- users/editProfile/editProfile.resolvers.js
+++ users/editProfile/editProfile.resolvers.js
@@ -1,17 +1,22 @@
-import client from '../../client';
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import client from '../../client';
+
 export default {
   Mutation: {
     editProfile: async (
       _,
-      { firstName, lastName, username, email, password: newPassword }
+      { firstName, lastName, username, email, password: newPassword, token }
     ) => {
+      const { id } = await jwt.verify(token, process.env.SECRET_KEY);
       let uglyPassword = null;
       if (newPassword) {
         uglyPassword = await bcrypt.hash(newPassword, 10);
       }
       const updatedUser = await client.user.update({
-        where: { id: 1 },
+        where: {
+          id,
+        },
         data: {
           firstName,
           lastName,
@@ -22,11 +27,11 @@ export default {
       });
       if (updatedUser.id) {
         return {
-          updatedUser: true,
+          ok: true,
         };
       } else {
         return {
-          updatedUser: false,
+          ok: false,
           error: 'Could not update profile.',
         };
       }
