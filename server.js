db6fa8177dfd8002d5dbf507e7898efcd4571bec
kiwoon kim <kisoon0947@gmail.com>
2022-11-25 21:37:43 +0900

    authentication part Two

--- server.js
+++ server.js
@@ -3,10 +3,14 @@ import { ApolloServer } from 'apollo-server';
 import schema from './schema';
 const server = new ApolloServer({
   schema,
+  context: {
+    token:
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNjY5Mzc5NTE5fQ.1T2B43hMUiUPH2RosVvSVil2h-4wwOUwsiYxNBkg8zI',
+  },
 });
 
 const PORT = process.env.PORT;
 
 server
   .listen(PORT)
-  .then(() => console.log(`🎊 Server is running on http://localhost:${PORT}/`));
+  .then(() => console.log(`🐯 포기하지 말자 🐯 [http://localhost:${PORT}/]`));
--- users/editProfile/editProfile.resolvers.js
+++ users/editProfile/editProfile.resolvers.js
@@ -6,8 +6,10 @@ export default {
   Mutation: {
     editProfile: async (
       _,
-      { firstName, lastName, username, email, password: newPassword, token }
+      { firstName, lastName, username, email, password: newPassword },
+      { token }
     ) => {
+      console.log(token);
       const { id } = await jwt.verify(token, process.env.SECRET_KEY);
       let uglyPassword = null;
       if (newPassword) {
