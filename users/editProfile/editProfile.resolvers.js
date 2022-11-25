e303154a237f5c62d4c8f8ace45a0e148609a02f
kiwoon kim <kisoon0947@gmail.com>
2022-11-25 20:22:08 +0900

    Edit Profile

--- users/editProfile/editProfile.resolvers.js
+++ users/editProfile/editProfile.resolvers.js
@@ -1,5 +1,35 @@
+import client from '../../client';
+import bcrypt from 'bcrypt';
 export default {
   Mutation: {
-    editProfile: () => console.log('hi'),
+    editProfile: async (
+      _,
+      { firstName, lastName, username, email, password: newPassword }
+    ) => {
+      let uglyPassword = null;
+      if (newPassword) {
+        uglyPassword = await bcrypt.hash(newPassword, 10);
+      }
+      const updatedUser = await client.user.update({
+        where: { id: 1 },
+        data: {
+          firstName,
+          lastName,
+          username,
+          email,
+          ...(uglyPassword && { password: uglyPassword }),
+        },
+      });
+      if (updatedUser.id) {
+        return {
+          updatedUser: true,
+        };
+      } else {
+        return {
+          updatedUser: false,
+          error: 'Could not update profile.',
+        };
+      }
+    },
   },
 };
