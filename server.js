bdb4ea56835c8f157d925498acb12c4b703d1682
kiwoon kim <kisoon0947@gmail.com>
2022-12-02 17:21:44 +0900

    Apollo server Set

--- server.js
+++ server.js
@@ -1,21 +1,24 @@
-require('dotenv').config();
-import { ApolloServer } from 'apollo-server';
-import { typeDefs, resolvers } from './schema';
-import { getUser } from './users/users.utils';
+require('dotenv').config()
+import express from 'express'
+import logger from 'morgan'
+import { ApolloServer } from 'apollo-server-express'
+import { typeDefs, resolvers } from './schema'
+import { getUser } from './users/users.utils'
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT
 const server = new ApolloServer({
-  typeDefs,
   resolvers,
+  typeDefs,
   context: async ({ req }) => {
     return {
       loggedInUser: await getUser(req.headers.token),
-    };
+    }
   },
-});
+})
 
-server
-  .listen(PORT)
-  .then(() =>
-    console.log(`🐯 절대 포기하지 말자 🐯 [http://localhost:${PORT}/]`)
-  );
+const app = express()
+app.use(logger('tiny'))
+server.applyMiddleware({ app })
+app.listen({ port: PORT }, () => {
+  console.log(`🚀포기하지말장 http://localhost:${PORT}/graphql 🐯`)
+})
