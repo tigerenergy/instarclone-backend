730efcd6b18f43c940165c577227f3237503129b
kiwoon kim <kisoon0947@gmail.com>
2022-12-09 07:12:25 +0900

    Search Users

---
+++ users/searchUsers/searchUsers.resolvers.js
@@ -0,0 +1,10 @@
+import client from '../../client'
+
+export default {
+  Query: {
+    searchUsers: async (_, { keyword }) =>
+      client.user.findMany({
+        where: { username: { startsWith: keyword.toLowerCase() } },
+      }),
+  },
+}
