a9663004a135151660cad0d4d9017b999c12a559
kiwoon kim <kisoon0947@gmail.com>
2022-12-09 22:23:55 +0900

    Edit Photo

---
+++ photos/editPhoto/editPhoto.resolvers.js
@@ -0,0 +1,31 @@
+import { protectedResolver } from '../../users/users.utils'
+import client from '../../client'
+export default {
+  Mutation: {
+    editPhoto: protectedResolver(
+      async (_, { id, caption }, { loggedInUser }) => {
+        const ok = await client.photo.findFirst({
+          where: {
+            id,
+            userId: loggedInUser.id,
+          },
+        })
+        if (!ok) {
+          return {
+            ok: false,
+            error: 'Photo not found.',
+          }
+        }
+        const photo = await client.photo.update({
+          where: {
+            id,
+          },
+          data: {
+            caption,
+          },
+        })
+        console.log(photo)
+      }
+    ),
+  },
+}
