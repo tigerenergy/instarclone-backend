a0b5c142a53feef89ee5318f69051fafb14c73bb
kiwoon kim <kisoon0947@gmail.com>
2022-12-13 20:25:59 +0900

    See (Read) Message

--- messages/messages.resolvers.js
+++ messages/messages.resolvers.js
@@ -1,5 +1,4 @@
 import client from '../client'
-
 export default {
   Room: {
     users: ({ id }) => client.room.findUnique({ where: { id } }).users(),
@@ -9,6 +8,21 @@ export default {
           roomId: id,
         },
       }),
-    unreadTotal: () => 0,
+    unreadTotal: ({ id }, _, { loggedInUser }) => {
+      if (!loggedInUser) {
+        return 0
+      }
+      return client.message.count({
+        where: {
+          read: false,
+          roomId: id,
+          user: {
+            id: {
+              not: loggedInUser.id,
+            },
+          },
+        },
+      })
+    },
   },
 }
