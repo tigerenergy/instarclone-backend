9c3d97687d4ea81de7113147e7ae1931ce07f330
kiwoon kim <kisoon0947@gmail.com>
2022-12-13 22:39:30 +0900

    Authenticating Subscriptions

--- server.js
+++ server.js
@@ -5,17 +5,34 @@ import logger from 'morgan'
 import { ApolloServer } from 'apollo-server-express'
 import { typeDefs, resolvers } from './schema'
 import { getUser } from './users/users.utils'
-
 const PORT = process.env.PORT
 const apollo = new ApolloServer({
   resolvers,
   typeDefs,
-  context: async ({ req }) => {
-    if (req) {
+  context: async (ctx) => {
+    if (ctx.req) {
       return {
-        loggedInUser: await getUser(req.headers.token),
+        loggedInUser: await getUser(ctx.req.headers.token),
+      }
+    } else {
+      const {
+        connection: { context },
+      } = ctx
+      return {
+        loggedInUser: context.loggedInUser,
       }
     }
+  },
+  subscriptions: {
+    onConnect: async ({ token }) => {
+      if (!token) {
+        throw new Error("You can't listen.")
+      }
+      const loggedInUser = await getUser(token)
+      return {
+        loggedInUser,
+      }
+    },
   },
 })
 
@@ -23,10 +40,8 @@ const app = express()
 app.use(logger('tiny'))
 apollo.applyMiddleware({ app })
 app.use('/static', express.static('uploads'))
-
 const httpServer = http.createServer(app)
 apollo.installSubscriptionHandlers(httpServer)
-
 httpServer.listen(PORT, () => {
   console.log(`🐯🐯🐯🐯🐯 💖 http://localhost:${PORT}/graphql 💖 🐷🐷🐷🐷🐷`)
 })
