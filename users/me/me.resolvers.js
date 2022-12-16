7a7e9d001be62ef7b838efc1ea4a9b710f7bb1dc
kiwoon kim <kisoon0947@gmail.com>
2022-12-16 16:32:17 +0900

    me Update

--- users/me/me.resolvers.js
+++ users/me/me.resolvers.js
@@ -4,7 +4,11 @@ import { protectedResolver } from '../users.utils'
 export default {
   Query: {
     me: protectedResolver((_, __, { loggedInUser }) =>
-      client.user.findUnique({ where: { id: loggedInUser.id } })
+      client.user.findUnique({
+        where: {
+          id: loggedInUser.id,
+        },
+      })
     ),
   },
 }
